e6a61d32567cc6603fdf94302f6d8b10e4a86f90
iacenik <108225596+iacenik@users.noreply.github.com>
2023-05-23 14:59:12 +0300

    Add files via upload

---
+++ Book_Store_API/Book_Store_API/Models/Students/CreateStudentRequestModel.cs
@@ -0,0 +1,10 @@
+﻿namespace Book_Store_API.Models.Students
+{
+    public class CreateStudentRequestModel
+    {
+        public string FullName { get; set; }
+        public int ClassId { get; set; }
+        public string Gender { get; set; }
+        public int Age { get; set; }
+    }
+}
---
+++ Book_Store_API/Book_Store_API/Services/Implementations/StudentCoursesService.cs
@@ -0,0 +1,25 @@
+﻿using Book_Store_API.Data;
+using Book_Store_API.Models.StudentCourses;
+using Book_Store_API.Services.Abstractions;
+
+namespace Book_Store_API.Services.Implementations
+{
+    public class StudentCoursesService:IStudentCoursesService
+    {
+        private readonly AppDbContext _context;
+      public StudentCoursesService(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        public Task CreateStudentCourses(CreateStudentCoursesRequestModel model)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<List<StudentCoursesViewModel>> GetAllStudentCourses()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
